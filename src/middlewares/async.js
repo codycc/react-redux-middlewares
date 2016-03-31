826a761c1c0d2128a5ae1f4bd55ce18e51d167ac
Cody Condon <codycondon50@gmail.com>
2016-03-31 14:03:17 -0400

    add fake json api information and fix middleware

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,8 +1,21 @@
 export default function({dispatch}){
   return next => action => {
-    console.log(action);
+    // if actions does not have payload
+    // or the payload does not have a .then property
+    // we dont care about it, send it on
+    if(!action.payload || !action.payload.then) {
+      return next(action);
+    }
+    action.payload
+    .then(function(response) {
+      // create a new action with the old type, but
+      //replace the promise with the response data
+      const newAction = {...action, payload: response};
+      dispatch(newAction);
+    });
+  }
 
-    next(action);
-  };
+  //make sure the actions promise resolves
+
 
 }
